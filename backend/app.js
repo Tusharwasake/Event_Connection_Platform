596f264bac724dbe359de4ad0c2ab29d40da74cc
Tushar Wasake <tusharwasake@gmail.com>
2025-01-17 23:28:41 +0530

    signup/login/refresh/logout

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import "dotenv/config";
+import { db } from "./database/datatase.js";
+import { router } from "./routes/authroutes.js";
+
+//.env files
+const SERVER_PORT = process.env.PORT;
+
+//app creation
+
+const app = express();
+
+// middlware
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser()); // Parse cookies
+
+//routes
+
+app.use("/user", router);
+
+//server Start
+app.listen(SERVER_PORT, () => {
+  try {
+    console.log(`server started at: http://localhost:${SERVER_PORT}`);
+    db(); // database connection
+  } catch (error) {
+    console.log("fail to start the server", message.error);
+  }
+});
---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import {
+  login,
+  signup,
+  refreshToken,
+  logout,
+} from "../controllers/authControllers.js";
+
+const router = Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/refresh", refreshToken);
+router.post("logout", logout);
+export { router };
